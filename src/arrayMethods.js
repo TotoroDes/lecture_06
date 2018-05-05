3cbf91fc1fa0ebc1ed09b1b6e5ea514f375537d5
Zorina Elena <38881469+TotoroDes@users.noreply.github.com>
2018-05-05 13:50:48 +0300

    Update arrayMethods.js

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,5 +1,41 @@
-window.every = function () {};
-window.some = function () {};
-window.forEach = function () {};
-window.filter = function () {};
-window.map = function () {};
+window.every = function (array, callback) {
+  var result = true;
+  for (var i = 0; i < array.length; i++) {
+    if (!callback(array[i], i, array)) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+window.some = function (array, callback) {
+  var result = false;
+  for (var i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      result = true;
+      break;
+    }
+  }
+  return result;
+};
+window.forEach = function (array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
+};
+window.filter = function (array, callback) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array) === true) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+};
+window.map = function(array, callback) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    result.push(callback(array[i], i, array));
+  }
+  return result;
+};
